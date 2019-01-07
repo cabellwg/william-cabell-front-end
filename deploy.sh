b47582b6af1cbff1f9150005f9934a1499e9a832
William Cabell <william16180@gmail.com>
2019-01-07 14:53:17 -0500

    Added portable pathfinding

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+
+# All hail the mighty StackOverflow
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
 
 # Clear out the old build
 cd flask_app
