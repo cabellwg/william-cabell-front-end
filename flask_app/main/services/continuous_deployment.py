1a0b1d834a2dfef3a4f42e9fbed244cd879b750d
William Cabell <william16180@gmail.com>
2019-01-07 14:11:23 -0500

    Made CD path portable to WSGI

--- flask_app/main/services/continuous_deployment.py
+++ flask_app/main/services/continuous_deployment.py
@@ -1,4 +1,7 @@
 import subprocess
+import os
+import flask_app
 
 def reload():
-    subprocess.call("./deploy.sh")
+    path = os.path.dirname(flask_app.__file__) + "/../"
+    subprocess.call(path + "deploy.sh")
