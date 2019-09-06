10176bb26940f3979e57654564645e739b4d52a3
William Cabell <william16180@gmail.com>
2019-09-06 13:04:15 -0400

    Fixed and streamlined LaTeX component

---
+++ src/components/eqn.vue
@@ -0,0 +1,34 @@
+<template>
+  <span></span>
+</template>
+
+<script>
+import katex from "katex";
+
+export default {
+  props: ["eqn", "display"],
+  mounted: async function() {
+    let mathOptions = {
+      colorIsTextColor: true,
+      displayMode: this.display ? true : false // no type safety!
+    };
+
+    if (this.display) {
+      this.$el.classList.add("katex__display");
+    } else {
+      this.$el.classList.add("katex__inline");
+    }
+
+    katex.render(this.eqn, this.$el, mathOptions);
+  }
+};
+</script>
+
+<style lang="sass">
+
+.katex
+  font-size: inherit
+
+.katex__display
+  display: block
+</style>
